c9f759bb7d0c82aa092edd13864742d4775f88f0
CarbonBeard <sharanov.pasha@gmail.com>
2017-05-24 02:39:00 +1000

    1. Schedules saves now. 2. Password validation is not

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -10,9 +10,15 @@ class SchedulesController < ApplicationController
   end
 
   def create
-    @parent.schedules << @schedule
-    @schedules = @parent.schedules
-    render 'index'
+    byebug
+    @parent.schedules.push Schedule.new(new_schedule_params)
+    if @parent.save
+      @schedules = @parent.schedules
+      render 'index'
+    else
+      @schedule = Schedule.new
+      render 'new'
+    end
   end
 
   def show
@@ -35,5 +41,10 @@ class SchedulesController < ApplicationController
         @parent = $1.classify.constantize.find(value)
       end
     end
+  end
+
+  def new_schedule_params
+    params.require(:schedule)
+      .permit(:title)
   end 
 end
