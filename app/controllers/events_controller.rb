ee59b0b077fe4da39fbea0963d06d7de6fd54147
eljee8 <eljee8@users.noreply.github.com>
2017-05-24 04:15:10 +1000

    Routing troubles

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,26 @@
 class EventsController < ApplicationController
-	def new
-		@event = @type.constantize.new
-	end
 
-	def show
+def new
+  @event = ScheduleEvent.new
+end
 
-	end
+def create
+  @user = User.find(id: params[:user_id])
+  @schedule = @user.schedules.find(id: params[:schedule_id])
+  @schedule << params[:event_type].constantize.new(new_schedule_event_params)
+end
+
+def show
+  @user = User.find(id: params[:user_id])
+  @schedule = @user.schedules.find(id: params[:schedule_id])
+end
+
+def new_schedule_event_params
+  params.require(:event)
+  .permit(:title,
+    :body,
+    :day,
+    :start_time,
+    :end_time)
+end
 end
